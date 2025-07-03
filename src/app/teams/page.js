5c9cdf7e823e5c6f174a26640a78f39571661e69
karan <itskaranraj009@gmail.com>
2025-07-03 18:58:53 +0530

    changes

--- src/app/teams/page.js
+++ src/app/teams/page.js
@@ -16,56 +16,7 @@ export default function Page() {
       <title>Our Team</title>
     </Head>
     <div className="min-h-screen w-full p-4 bg-[#0D1116] flex flex-col gap-28">
-      {/* <div>
-        <StyledText
-          primary="#007cf0"
-          secondary="#00dfd8"
-          className="text-[36px] font-bold flex flex-col items-center mb-4"
-        >
-          Batch 2K20
-        </StyledText>
-        <div className={style.cardContainer}>
-          {TeamData2k20.map((item) => {
-            {item.imgSrc=item.imgSrc.replace('.jpg','.png')}
-            
-            return (
-              <TeamCard
-                name={item.name}
-                pos={item.pos}
-                imgSrc={'/teams/2K20/'+item.imgSrc}
-                lazyImgSrc={'/teams/2K20/lazy/'+item.imgSrc}
-                socials={item.socials}
-                key={item.name}
-              />
-            );
-          })}
-        </div>
-      </div> */}
-     {/*<div>
-        <StyledText
-          primary="#007cf0"
-          secondary="#00dfd8"
-          className="text-[36px] font-bold flex flex-col items-center mb-4"
-        >
-          Batch 2K21
-        </StyledText>
-        <div className={style.cardContainer}>
-          {TeamData2k21.map((item) => {
-            return (
-              <TeamCard
-                name={item.name}
-                pos={item.pos}
-                imgSrc={'/teams/2K21/'+item.imgSrc}
-                lazyImgSrc={'/teams/2K21/lazy/'+item.imgSrc}
-                socials={item.socials}
-                key={item.name}
-              />
-            );
-          })}
-        </div>
-      </div>*/}
-      
-      <div>
+       <div>
         <StyledText
           primary="#007cf0"
           secondary="#00dfd8"
